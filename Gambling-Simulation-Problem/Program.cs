4f485ba32d350cebf162ca9698abb09cb1e5c561
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-11 18:12:01 +0530

    [AKSHAY] added win or loss for 50 and 150

--- Gambling-Simulation-Problem/Program.cs
+++ Gambling-Simulation-Problem/Program.cs
@@ -4,10 +4,38 @@ namespace Gambler
 {
 	class Program
 	{
+		public const int WIN = 1;
+		public const int STAKE = 100;
+		public const int BET = 1;
+		public static void callMethod()
+		{
+			int amountLeft = 100;
+			//int numOfBets = 0;
+			Random random = new Random();
+			while (amountLeft != amountLeft / 2 || amountLeft == amountLeft + 50)
+			{
+				//numOfBets++;
+				int win_loss = random.Next(0, 2);
+				if (win_loss == WIN)
+				{
+					amountLeft = amountLeft + BET;
+					Console.WriteLine(amountLeft);
+					if (amountLeft == 150)
+						break;
+				}
+				else
+				{
+					amountLeft = amountLeft - BET;
+					Console.WriteLine(amountLeft);
+					if (amountLeft == 50)
+						break;
+				}
+			}
+			Console.WriteLine("The amount I have left after betting is: " + amountLeft);
+		}
 		static void Main(string[] args)
 		{
-			int STAKE = 100;
-			int BET = 1;
+			callMethod();
 			Console.ReadLine();
 		}
 	}

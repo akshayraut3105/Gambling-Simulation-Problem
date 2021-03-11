8684245edee1885025fd5f3f503c044087f6445b
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-11 18:16:13 +0530

    [AKSHAY] added calculation for 20 days

--- Gambling-Simulation-Problem/Program.cs
+++ Gambling-Simulation-Problem/Program.cs
@@ -7,20 +7,41 @@ namespace Gambler
 		public const int WIN = 1;
 		public const int STAKE = 100;
 		public const int BET = 1;
+		public static void callMethod()
+		{
+			int totalamount = 0;
+			//int numOfBets = 0;
+			Random random = new Random();
+			for (int i = 0; i <= 19; i++)
+			{
+				int amountLeft = 100;
+				while (amountLeft != amountLeft / 2 || amountLeft == amountLeft + 50)
+				{
+					//numOfBets++;
+					int win_loss = random.Next(0, 2);
+					if (win_loss == WIN)
+					{
+						amountLeft = amountLeft + BET;
+						//Console.WriteLine(amountLeft);
+						if (amountLeft == 150)
+							break;
+					}
+					else
+					{
+						amountLeft = amountLeft - BET;
+						//Console.WriteLine(amountLeft);
+						if (amountLeft == 50)
+							break;
+					}
+				}
+				Console.WriteLine("The amount I have left after betting is: " + amountLeft + " on Day#" + (i + 1));
+				totalamount += amountLeft;
+			}
+			Console.WriteLine("Total Amount he has after 20 days is: " + totalamount);
+		}
 		static void Main(string[] args)
 		{
-			int amountLeft = 100;
-			Random random = new Random();
-			int win_loss = random.Next(0,2);
-			if (win_loss == WIN)
-			{
-				amountLeft = STAKE + BET;
-			}
-			else
-			{
-				amountLeft = STAKE - BET;
-			}
-			Console.WriteLine("The amount I have lefft after betting is: " +amountLeft);
+			callMethod();
 			Console.ReadLine();
 		}
 	}
